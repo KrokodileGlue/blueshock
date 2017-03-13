4efbbb52820a707cd9fb9fad19f35a4fbc1bd8b2
KrokodileGlue <taylorwest111@gmail.com>
2017-03-13 10:00:39 -0700

    SDL2 cmake variable => SDL2_LOCATION

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,17 @@
 #include <GL/glew.h>
 
 const GLchar* vertexShaderSource = "#version 130\n"
-    "in vec3 position;\n"
-    "void main()\n"
-    "{\n"
-    "gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
-    "}\0";
+"in vec3 position;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+"}\0";
 const GLchar* fragmentShaderSource = "#version 130\n"
-    "out vec4 color;\n"
-    "void main()\n"
-    "{\n"
-    "color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\n\0";
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
 
 int main(int argc, char* argv[])
 {
